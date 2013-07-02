3521af1d5774d89840b50f6ff435d163d048a8e6
Deepak Giridharagopal <deepak@puppetlabs.com>
2013-07-02 12:34:55 -0600

    Add signing key

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/grimradical/tableflisp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :signing {:gpg-key "CB150440"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
