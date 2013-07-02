d08383ae5a5b02e28bfca17d7b159801f5a02740
Deepak Giridharagopal <deepak@puppetlabs.com>
2013-07-01 19:16:59 -0600

    Update project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tableflisp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Unicode weapons for a more civilized age"
+  :url "https://github.com/grimradical/tableflisp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
